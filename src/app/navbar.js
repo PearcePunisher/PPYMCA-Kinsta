d58a6186294cf50d79d4e1435a09d0b6c3e82b4a
Riley Pearce <riley@roguesalad.co>
2024-03-07 12:46:18 -0700

    Add new fonts and pages, update navbar logo

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -5,35 +5,39 @@ import { usePathname } from 'next/navigation';
 const NavLinks = [
 	{ id: 1, name: 'Home', path: '/' },
 	{ id: 2, name: 'Blog', path: '/blog' },
+  { id: 3, name: 'Pages', path: '/pages' },
+  { id: 4, name: 'Contact', path: '/contact' },
 ];
 
 const Navbar = () => {
 	const pathname = usePathname();
 	const isActive = (path) => path === pathname;
 
-	return (
-		<nav>
-			<div className="navbar">
-				<Link href="/">
-					<span className="logo">NextWp</span>
-				</Link>
-				<ul>
-					{NavLinks.map((link) => {
-						return (
-							<li key={link.id}>
-								<Link
-									href={link.path}
-									className={isActive(link.path) ? 'active' : ''}
-								>
-									{link.name}
-								</Link>
-							</li>
-						);
-					})}
-				</ul>
-			</div>
-		</nav>
-	);
+  return (
+    <nav>
+      <div className="navbar">
+        <Link href="/">
+          <span className="logo">
+            <img src="http://ymcanext.kinsta.cloud/wp-content/uploads/2024/03/ymca-3.svg" alt="YMCA Logo" />
+          </span>
+        </Link>
+        <ul>
+          {NavLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <Link
+                  href={link.path}
+                  className={isActive(link.path) ? 'active' : ''}
+                >
+                  {link.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
