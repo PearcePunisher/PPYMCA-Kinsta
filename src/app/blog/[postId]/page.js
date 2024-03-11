9e730dfe48afc04a5b84dfea51b1935524263284
skully1333 <skully1333@gmail.com>
2024-03-11 21:05:39 +0000

    Added Comments for clairity

--- src/app/blog/[postId]/page.js
+++ src/app/blog/[postId]/page.js
@@ -1,16 +1,20 @@
+// Indicates that the page uses dynamic routing parameters
 export const dynamicParams = true;
 
+// Fetches a list of all posts from the WordPress API
 export async function generateStaticParams() {
 	const response = await fetch(
 		`${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}/posts`
 	);
 	const posts = await response.json();
 
+	// Returns an array of objects containing the post IDs as strings
 	return posts.map((post) => ({
 		postId: post.id.toString(),
 	}));
 }
 
+// Fetches a single post from the WordPress API based on the provided post ID
 async function getSinglePost(postId) {
 	const response = await fetch(
 		`${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}/posts/${postId}`
@@ -19,13 +23,17 @@ async function getSinglePost(postId) {
 	return post;
 }
 
+// Renders the single blog page
 const page = async ({ params }) => {
+	// Fetches the post data for the current post ID
 	const post = await getSinglePost(params.postId);
 
+	// If the post is not found, display a loading message
 	if (!post) {
 		return <div>Loading...</div>;
 	}
 
+	// Renders the blog post content
 	return (
 		<div className="single-blog-page">
 			<h2>{post.title.rendered}</h2>
@@ -36,4 +44,5 @@ const page = async ({ params }) => {
 	);
 };
 
+// Exports the page component
 export default page;
--- src/app/pages/[pageId]/page.js
+++ src/app/pages/[pageId]/page.js
@@ -1,16 +1,20 @@
+// Indicates that this page uses dynamic routing
 export const dynamicParams = true;
 
+// Fetches a list of all pages from the WordPress API
 export async function generateStaticParams() {
 	const response = await fetch(
 		`${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}/pages`
 	);
 	const pages = await response.json();
 
+	// Returns an array of page IDs
 	return pages.map((page) => ({
 		pageId: page.id.toString(),
 	}));
 }
 
+// Fetches a single page from the WordPress API based on the page ID
 async function getSinglepage(pageId) {
 	const response = await fetch(
 		`${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}/pages/${pageId}`
@@ -19,13 +23,17 @@ async function getSinglepage(pageId) {
 	return page;
 }
 
+// Renders the page component
 const page = async ({ params }) => {
+	// Fetches the page data for the current page ID
 	const page = await getSinglepage(params.pageId);
 
+	// If the page data is not found, display a loading message
 	if (!page) {
 		return <div>Loading...</div>;
 	}
 
+	// Renders the page component with the page data
 	return (
 		<div className="single-blog-page">
 			<h2>{page.title.rendered}</h2>
@@ -36,4 +44,5 @@ const page = async ({ params }) => {
 	);
 };
 
+// Exports the page component
 export default page;
